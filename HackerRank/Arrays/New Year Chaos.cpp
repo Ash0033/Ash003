b0cc54292ec43d817408f8550b635afba1d911fa
Ash0033 <ashot2003babayan@gmail.com>
2025-10-15 00:37:48 +0400

    fix

--- HackerRank/Arrays/New Year Chaos.cpp
+++ HackerRank/Arrays/New Year Chaos.cpp
@@ -1,9 +1,16 @@
  int bribes = 0;
     int n = q.size();
-
-    for (int i = 0; i < n; i++) {
+       for (int i = 0; i < n; i++) {
         // Եթե մարդը իր սկզբնական տեղից ավելի քան 2 դիրքով առաջ է անցել
         if (q[i] - (i + 1) > 2) //q[i]-ն սկզբնական դիրք, (i+1)-ն վերջնական
          {
             cout << "Too chaotic" << endl;
-            return;     }}
+            return;     }
+           // Ստուգում ենք՝ քանի հոգու է նա bribed արել
+        for (int j = max(0, q[i] - 2); j < i; j++) {
+            if (q[j] > q[i]) bribes++;
+        }
+    }
+    cout << bribes << endl;
+
+ 
